9e1a75a8004a36a68504a3cecafcdbfb593fa86a
Chet Murthy <chetsky@gmail.com>
2020-12-02 13:58:16 -0800

    meh
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   pa_unique/META.pl
    #
    # Untracked files:
    #	tests/include_ml
    #

--- pa_unique/META.pl
+++ pa_unique/META.pl
@@ -26,6 +26,7 @@ description = "pa_ppx_unique deriver"
   package "link" (
   requires(byte) = "camlp5,pa_ppx.deriving_plugins.show.link,pa_ppx.params_runtime"
   archive(byte) = "pa_deriving_unique.cmo"
+  archive(native) = "pa_deriving_unique.cmx"
   )
 
   requires = "pa_ppx_unique.runtime"
